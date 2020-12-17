834d26fd3ff652f5e83f88922c1e058906bcc544
kansai-gamer <takumi@tutanota.de>
2020-12-17 23:38:00 +0900

    1217

--- ghm.py
+++ ghm.py
@@ -2,20 +2,28 @@ import os
 from ghm_class import GhmRepo
 
 if not os.path.isfile("gh_token.txt"):
-    token = input("pls type your Github token")
+    #ユーザーIDも必要ぽいので同じファイルに格納する
+    write = []
+    write.append(input("pls type your Github token:"))
+    write.append(input("pls type your Github ID:"))
     with open("gh_token.txt", mode="w", encoding="utf-8") as f:
-        f.write(token)
+        for token_and_user in write:
+            f.write(token_and_user+"\n")
 
 with open("gh_token.txt", mode="r", encoding="utf-8") as f:
+    token = f.readlines()
     #インスタンス生成
-    ghm = GhmRepo(f.read())
+    ghm = GhmRepo(token[0].strip())
+    #↑改行コードの削除
+    #↓デバッグ用
+    # print(token[0].strip())
 
-print("welcome to Github Manager")
+print("welcome to Github Manager\nplz select to repo")
 for number, repos in enumerate(ghm.get_repo()):
     print(number + 1, ":", repos)
-usertype = input("plz select to repo:")
+usertype = input("waiting:")
 
 
-print(ghm.read_folder_names("kansai-gamer","bash"))
+# print(ghm.read_folder_names("kansai-gamer","bash"))
 
 # ghm.read_readme("kansai-gamer","bash")
