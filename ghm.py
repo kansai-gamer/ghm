1e9b91757f14ad05bf51b7c804355c24ad6dd5eb
kansai-gamer <me@japan-is.fun>
2020-12-21 14:05:13 +0900

    1221

--- ghm.py
+++ ghm.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from ghm_class import GhmRepo
 
 if not os.path.isfile("gh_token.txt"):
@@ -15,44 +16,54 @@ with open("gh_token.txt", mode="r", encoding="utf-8") as f:
     #ファイルをリストにして取得
     token = f.readlines()
     #インスタンス生成
-    ghm = GhmRepo(token[0].strip())
+    ghm = GhmRepo(token[1].strip())
                             #↑改行コードの削除
-    #↓デバッグ用
-    # print(token[0].strip())
 
 print("welcome to Github Manager")
+#デバッグ用 ID読み取り確認
+# print(token[0].strip())
+#デバッグ用 トークン読み取り確認
+# print(token[1].strip())
+
+def Listen_repo(ghm):
+
+    while True:
+
+        print("pls select to repo")
+        for number, repos in enumerate(ghm.get_repo()):
+            print(number + 1, ":", repos)
+        user_type_repo = input("waiting:")
+        if user_type_repo.isdecimal():
+            # user_type_repo_int = int(user_type_repo)
+            return int(user_type_repo)
+        else:
+            # print('\033[1m' + 'pls type number')
+            print("\033[1m" + "pls type number\033[0m")
+
+
+user_type_repo_int = Listen_repo(ghm)
 
 while True:
 
-    print("pls select to repo")
-    for number, repos in enumerate(ghm.get_repo()):
-        print(number + 1, ":", repos)
-    user_type_repo = input("waiting:")
-    if user_type_repo.isdecimal():
-        user_type_repo_int = int(user_type_repo)
-        break
-    else:
-        print('\033[1m' + 'pls type number')
-
-
-while True:
-
+    # print("\033[1m" + "pls select\033[0m")
     print("pls select")
-    options = ["clone", "Read README.md", "View folder"]
+    options = ["clone", "Read README.md", "View folder", "back to select repo"]
     for number, option in enumerate(options):
         print(number + 1, ":", option)
     user_type_option = input("waiting:")
     if user_type_option.isdecimal():
         user_type_option_int = int(user_type_option)
     else:
-        print('\033[1m' + "pls type number")
+        print("\033[1m" + "pls type number\033[0m")
         
     if user_type_option_int == 1:
-        pass
+        command = 'git clone','https://github.com/'+token[0].strip()+'/'+ghm.get_repo()[user_type_repo_int - 1]
+        subprocess.call(command)
     elif user_type_option_int == 2:
-        print(ghm.read_readme(token[1].strip(),ghm.get_repo()[user_type_repo_int - 1]))
+        print(ghm.read_readme(token[0].strip(),ghm.get_repo()[user_type_repo_int - 1]))
         continue
     elif user_type_option_int == 3:
-        print(ghm.read_folder_names(token[1].strip(),ghm.get_repo()[user_type_repo_int - 1]))
+        print(ghm.read_folder_names(token[0].strip(),ghm.get_repo()[user_type_repo_int - 1]))
         continue
-
+    elif user_type_option_int == 4:
+        user_type_repo_int = Listen_repo(ghm)
